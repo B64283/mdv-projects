6e23173aa0fce11cb17a04b4c4be138d10affbcb
B64283 <mattdarke@fullsail.edu>
2013-05-17 01:52:37 -0700

    boolean

--- Darke_Matthew_Assignment2.js
+++ Darke_Matthew_Assignment2.js
@@ -1,9 +1,97 @@
 //alert("JavaScript works!");
-//SDI 1305 Projec2 
+
 //By Matthew Darke
+//SDI 1304 
+//Projec2 
 
-var ContestNames = [ "Pie", "Hotdog", "Pizza" ];
-var MinutesPerContest = 15, 20, 30;
-var kidsNames = [ "Jessica," "Allie," "Nick" ];
+//Variables
+var carnivalName = "Fall County Carnival";
+var ContestNames = [ 
+    "Pie eating contest",
+    "Hotdog eating contest",
+    "Pizza eating contest" ];
+var KidsName = "Allison";
+var GoalsPerContest = [25, 45, 55];
+var FoodLastYear = [20, 40, 50];
+var IntroStory = "Allison is going to the " + carnivalName +  " Last year she won three eating contests ";
+
+// Procedure
+function eatingcontest(enter) {
+     if (enter === true) { 
+	     console.log(" This year " + KidsName + " is entering an eating contest at the " + carnivalName + " to try to beat her record ")
+     } else { 
+         console.log (" theres always next year for " + KidsName + " to try to beat her record. ") 
+	     }
+};	     
 
 
+
+
+
+
+//Boolean
+function AccomplishesGoalFor(Pie, HotDog, Pizza ) {
+	if (Pie && !(HotDog || Pizza)) {
+		console.log(" the eating contest champ is " + KidsName + " And all she could do now is puke " ) 
+	} els {
+	    Console.log ("she goes home with a belly ache!" );
+    }
+    return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for loop
+//var ContestNames = [ "Pie eating contest", "Hotdog eating contest", "Pizza eating contest" ],
+   // GoalsPerContest = [ 25, 45, 55 ],
+//for (var ContestNumber = 0; ContestNumber < ContestNames.length; ContestNumber ++) {
+   // var ContestName = ContestNames[ContestNumber],
+	  //  GoalThisContest = GoalsPerContest[ContestNumber];
+    //console.log( " So " + KidsName + " Starts the " + ContestName + " to reach a goal of "  + GoalThisContest + " to win.");
+  //  for (var Kids = 0; Kids < KidsName; Kids ++) {
+  //  for (var Goal = 0; Goal < GoalsPerContest; Goal ++)
+  //  var Goalremain = GoalThisContest - Goal;
+	//console.log(Goal + " finish, " + Goalremain + " Left! ");   
+   //// }
+//console.log( "And " + KidsName + " reached goal and won the " + ContestName + ".");
+//}
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
